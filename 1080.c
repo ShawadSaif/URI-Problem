92d01acdef59ecdddccc54af66f994dfd312abe0
Shaif Al Shad <99761472+ShawadSaif@users.noreply.github.com>
2022-05-10 00:02:03 +0600

    Add files via upload

---
+++ 1080.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int i,p,n,h=0;
+    for(i=1;i<=100;i++){
+        scanf("%d",&n);
+        if(n>h){
+            h=n;
+            p=i;
+        }
+    }
+    printf("%d\n%d\n",h,p);
+    return 0;
+}
